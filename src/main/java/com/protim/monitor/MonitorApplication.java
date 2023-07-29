98bafd70f9bc2af23ed722ae9405f69e1cc7775d
Vishwa Protim <vishwaprotimbanerjee93@gmail.com>
2023-07-29 19:17:04 +0530

    Adding Repository configuration for HTTP security

--- src/main/java/com/protim/monitor/MonitorApplication.java
+++ src/main/java/com/protim/monitor/MonitorApplication.java
@@ -1,13 +1,37 @@
 package com.protim.monitor;
 
+import com.protim.monitor.entity.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @SpringBootApplication
 public class MonitorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer()
+	{
+		return RepositoryRestConfigurer.withConfig(config -> {
+			// Setting base path /student to /api/student
+			config.setBasePath("/api");
+
+			// Spring Data REST hides IDs by default for security.
+			// Exposing them for demonstration.
+			config.exposeIdsFor(Student.class);
+
+			// Disabling PATCH -
+			// PATCH is unsafe HTTP update as it needs only specific fields for update.
+			// PUT on the other hand replaces the entire resource, even if they are not modified.
+			config.getExposureConfiguration().disablePatchOnItemResources();
+
+			// Disabling resource creation (201 Created) using PUT
+			config.getExposureConfiguration().disablePutForCreation();
+		});
 	}
 
 }
--- src/main/java/com/protim/monitor/repository/StudentRepository.java
+++ src/main/java/com/protim/monitor/repository/StudentRepository.java
@@ -3,9 +3,30 @@ package com.protim.monitor.repository;
 import com.protim.monitor.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.math.BigInteger;
 
+
+/**
+ * For more information on Spring Data REST, check this
+ * <a href="https://docs.spring.io/spring-data/rest/docs/current/reference/html/#repository-resources">documentation</a>.<br>
+ * <br>
+ * Spring Data REST supports only below delete methods:
+ * <ul>
+ *     <li>delete(T)</li>
+ *     <li>delete(ID)</li>
+ *     <li>delete(Iterable)</li>
+ * </ul>
+ * Note that deleteAll() is not allowed.
+ *
+ * @author vishwaprotim
+ */
 @RepositoryRestResource(collectionResourceRel = "student", path = "student")
 public interface StudentRepository extends JpaRepository<Student, BigInteger> {
+
+    // Disabling delete using entity method
+    @Override
+    @RestResource(exported = false)
+    void delete(Student entity);
 }
